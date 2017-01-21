b6e1205dd327a013fd66f3657468f8251dff6256
Alex <yepishevsanya@mail.ru>
2017-01-21 15:06:40 +0200

    Add NarrowingCasting

---
+++ src/main/java/com/chudilka1/app/NarrowingCasting.java
@@ -0,0 +1,16 @@
+package com.chudilka1.app;
+
+
+public class NarrowingCasting {
+    int int1 = 0;
+    float float1 = 3.14159f;
+
+    public void narrowing() {
+        int1 = (int) float1;
+        System.out.println(int1);
+    }
+    public static void main(String[] args) {
+        NarrowingCasting narrowingCasting = new NarrowingCasting();
+        narrowingCasting.narrowing();
+    }
+}
