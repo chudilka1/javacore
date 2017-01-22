166dd53c3c156057bb3cc37349b7988ca4d72d03
Alex <yepishevsanya@mail.ru>
2017-01-22 12:42:02 +0200

    Switch variables for conversion

--- src/main/java/com/chudilka1/app/utils/convertors/PrimitiveConvertor.java
+++ src/main/java/com/chudilka1/app/utils/convertors/PrimitiveConvertor.java
@@ -2,19 +2,19 @@ package com.chudilka1.app.utils.convertors;
 
 public class PrimitiveConvertor {
     char char1 = 'x';
-    int int1 = 0;
-    float float1 = 0f;
+    int int1 = 65;
+    float float1 = 78.0f;
 
     public void floatToChar() {
-        float1 = char1;
+        char1 = (char) float1;
         System.out.println("Input float value is " + float1 + ". Output char value is " + char1);
     }
     public void intToChar() {
-        int1 = char1;
+        char1 = (char) int1;
         System.out.println("Input int value is " + int1 + ". Output char value is " + char1);
     }
     public void charToInt() {
-        char1 = (char) int1;
+        int1 = (int) char1;
         System.out.println("Input char value is " + char1 + ". Output int value is " + int1);
     }
 }
