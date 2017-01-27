d86da74f21f9f97ebb54b2d2934f0d03f10ceb0d
Alex <yepishevsanya@mail.ru>
2017-01-27 12:56:32 +0200

    Move Lesson3Runner into runners package

---
+++ src/main/java/com/chudilka1/app/runners/homework/lesson2/Lesson3Runner.java
@@ -0,0 +1,11 @@
+package com.chudilka1.app.runners.homework.lesson2;
+import com.chudilka1.app.homework.lesson2.convertors.PrimitiveConvertor;
+
+public class Lesson3Runner {
+    public static void main(String[] args) {
+        PrimitiveConvertor convertor = new PrimitiveConvertor();
+        convertor.floatToChar();
+        convertor.intToChar();
+        convertor.charToInt();
+    }
+}
