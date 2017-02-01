33c0bec4482bc8d6648e97265abdaace55124339
Alex <yepishevsanya@mail.ru>
2017-02-01 20:29:46 +0200

    Rewrite PrimitiveConvertor (class & runner)

--- src/main/java/com/chudilka1/app/homework/lesson2/convertors/PrimitiveConvertor.java
+++ src/main/java/com/chudilka1/app/homework/lesson2/convertors/PrimitiveConvertor.java
@@ -1,20 +1,23 @@
 package com.chudilka1.app.homework.lesson2.convertors;
 
 public class PrimitiveConvertor {
-    char char1 = 'x';
-    int int1 = 65;
-    float float1 = 78.0f;
+    public float float1;
+    public int int1;
+    public char char1;
 
-    public void floatToChar() {
-        char1 = (char) float1;
+    public static char floatToChar(float float1) {
+        char char1 = (char) float1;
         System.out.println("Input float value is " + float1 + ". Output char value is " + char1);
+        return char1;
     }
-    public void intToChar() {
-        char1 = (char) int1;
+    public static char intToChar(int int1) {
+        char char1 = (char) int1;
         System.out.println("Input int value is " + int1 + ". Output char value is " + char1);
+        return char1;
     }
-    public void charToInt() {
-        int1 = (int) char1;
+    public static int charToInt(char char1) {
+        int int1 = (int) char1;
         System.out.println("Input char value is " + char1 + ". Output int value is " + int1);
+        return int1;
     }
 }
--- src/main/java/com/chudilka1/runners/homework/lesson2/PrimitiveConvertorRunner.java
+++ src/main/java/com/chudilka1/runners/homework/lesson2/PrimitiveConvertorRunner.java
@@ -1,11 +1,14 @@
 package com.chudilka1.runners.homework.lesson2;
 import com.chudilka1.app.homework.lesson2.convertors.PrimitiveConvertor;
 
-public class Lesson3Runner {
+public class PrimitiveConvertorRunner {
     public static void main(String[] args) {
         PrimitiveConvertor convertor = new PrimitiveConvertor();
-        convertor.floatToChar();
-        convertor.intToChar();
-        convertor.charToInt();
+        convertor.float1 = 78.098f;
+        convertor.int1 = 88;
+        convertor.char1 = 'x';
+        convertor.floatToChar(convertor.float1);
+        convertor.intToChar(convertor.int1);
+        convertor.charToInt(convertor.char1);
     }
 }
