c851b7576fd1d042bfbd73293a906b541fe74011
Aleksandr Yepishev <aleksandr.yepishev@uvoteam.com>
2017-02-13 12:21:50 +0200

    Add minor changes to lesson4 (classwork, homework, runners) + parametrized tests

--- src/main/java/com/chudilka1/app/classwork/lesson4/RightAngledTriangle.java
+++ src/main/java/com/chudilka1/app/classwork/lesson4/RightAngledTriangle.java
@@ -2,7 +2,7 @@ package com.chudilka1.app.classwork.lesson4;
 
 
 public class RightAngledTriangle {
-    static private boolean[] result = {false, false};
+    private static boolean[] result = {false, false};
 
     public static boolean[] isTriangleCalculations(double side1, double side2, double side3) {
         if ((side1 * side1 == side2 * side2 + side3 * side3)||(side2 * side2 == side1 * side1 + side3 * side3)||(side3 * side3 == side1 * side1 + side2 * side2)) {
